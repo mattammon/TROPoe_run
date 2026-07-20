86dcad3d0e7cb02c84d9c144d3def5649a30f82c
svcammon <svcammon@clamps-1-8.winstorm.nssl>
2026-07-20 13:01:44 -0500

    Added recursive for many bands

--- MANY_TROPoe.py
+++ MANY_TROPoe.py
@@ -11,20 +11,23 @@ df = pd.read_csv(f'{SCRIPT_DIR}/{cc_type_title}_sgp_sounding_times.csv')
 dates = df['date']
 tms = df['hr_dec']
 
+bands = [1,2,3,4,5,6,7,8,9,10,11,12]
+
 ##########################################
 
-for i,d in enumerate(dates[:20]):
-    write_vip(data_path='/sgp/clear_sky_days',
-              irs_channel=2,
-              band=12)
-    date = d
-    start_hr = tms[i]
-    end_hr = tms[i]
-    vip_file = f'{VIP_DIR}/curr_VIP.txt'
-    prior_file = 'prior.MIDLAT.nc'
-    verbose = '1'
-    data_root = DATA_DIR
+for b in bands:
+    for i,d in enumerate(dates[:20]):
+        write_vip(data_path='/sgp/clear_sky_days',
+                irs_channel=2,
+                band=b)
+        date = d
+        start_hr = tms[i]
+        end_hr = tms[i]
+        vip_file = f'{VIP_DIR}/curr_VIP.txt'
+        prior_file = 'prior.MIDLAT.nc'
+        verbose = '1'
+        data_root = DATA_DIR
 
-    os.system(f'python TROPoe.py {date} {vip_file} {data_root}/{prior_file} --shour={start_hr} --ehour={end_hr} --verbose={verbose}')
+        os.system(f'python TROPoe.py {date} {vip_file} {data_root}/{prior_file} --shour={start_hr} --ehour={end_hr} --verbose={verbose}')
 
-    print('####### DONE #######')
+        print('####### DONE #######')
